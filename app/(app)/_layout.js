02b47d4746c722b55dcd1e2bfc5add9653b38668
Lucas <sdo23lucas@gmail.com>
2024-06-16 15:13:22 -0300

    +OContext +MainLayout

---
+++ app/(app)/_layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function _layout() {
+    return (
+        <View style={{paddingTop: 40}} className="bg-red-500 pt-20">
+            <Text className="text-3xl text-center">_layout</Text>
+        </View>
+    )
+}
