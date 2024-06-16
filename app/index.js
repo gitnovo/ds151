744d09bf19f8e95614cb196e639ce7d88f1a66de
Lucas <sdo23lucas@gmail.com>
2024-06-15 23:18:14 -0300

    commit

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 
 export default function Home() {
     return (
-        <View>
+        <View style={{paddingTop: 40}} className="bg-red-200">
             <Text>Home</Text>
         </View>
     )
